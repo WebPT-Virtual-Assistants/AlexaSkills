67cdcbb9f552b0bcd881d4b3177fff5f46622656
QuanMLe <qmle1>
2018-10-03 20:28:16 -0700

    Changed the blood pressure to use 'over'

--- BloodPressure.py
+++ BloodPressure.py
@@ -7,6 +7,6 @@ with open('CSV/BloodPressure.csv', mode='wb') as csv_file:
     writer.writeheader()
     for x in range(70,191):
         for y in range(40,101):
-            bp = "%d/%d mmHg"%(x,y)
+            bp = "%d over %d mmHg"%(x,y)
             writer.writerow({'blood_pressure': bp})
     
